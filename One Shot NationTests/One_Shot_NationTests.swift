ca9d36ed42f66e0ee22ce1795b3d2e9aeb3eeade
Julio Maldonado <Julio.A.Maldo@gmail.com>
2017-03-14 11:54:48 -0500

    Initial Commit

---
+++ One Shot NationTests/One_Shot_NationTests.swift
@@ -0,0 +1,36 @@
+//
+//  One_Shot_NationTests.swift
+//  One Shot NationTests
+//
+//  Created by Julio Maldonado on 3/14/17.
+//  Copyright © 2017 TrackingPoint. All rights reserved.
+//
+
+import XCTest
+@testable import One_Shot_Nation
+
+class One_Shot_NationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
