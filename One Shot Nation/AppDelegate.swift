1d5a018bb385bde40b515874ad4eb4dad77633f1
Julio Maldonado <Julio.A.Maldo@gmail.com>
2017-03-14 13:36:58 -0500

    FB login Button added

--- One Shot Nation/AppDelegate.swift
+++ One Shot Nation/AppDelegate.swift
@@ -24,6 +24,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+      
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation] as! String!)
+        
+        return handled
+        
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
