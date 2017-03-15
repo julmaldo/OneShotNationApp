24609f9e0f3c41e0f9630dbf1883c5b9efdd668f
Julio Maldonado <Julio.A.Maldo@gmail.com>
2017-03-15 11:59:31 -0500

    Added user creation to firebase app

--- One Shot Nation/ViewController.swift
+++ One Shot Nation/ViewController.swift
@@ -8,17 +8,62 @@
 
 import UIKit
 import FBSDKLoginKit
+import Firebase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let loginButton = FBSDKLoginButton()
-        
         view.addSubview(loginButton)
         loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
-      
+        loginButton.delegate = self
+        loginButton.readPermissions = ["email", "public_profile"]
+        
+    
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print(error)
+            return
+        }
+        showEmailAddress()
+        
+    }
+    
+    func showEmailAddress(){
+        let accessToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accessToken?.tokenString else
+        {return }
+        let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        
+        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
+            if error != nil{
+                print("Something went wrong with our FB user:", error ?? "")
+                return
+            }
+            
+            print("Successfully logged in with our user: ",user ?? "")
+        })
+        
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, err) in
+            
+            if err != nil{
+                print("Failed to start graph request:", err ?? "")
+                return
+            }
+            print(result ?? "" )
+        }
+
     }
 }
 
---
+++ Pods/Target Support Files/Pods-OneShotNation/Pods-OneShotNation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OneShotNation : NSObject
+@end
+@implementation PodsDummy_Pods_OneShotNation
+@end
